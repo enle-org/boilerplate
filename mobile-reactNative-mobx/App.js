536574eb7ce6f27e689cb8648357972cf3dd2465
Ekpo David <dave170e71@gmail.com>
2019-12-04 06:13:09 +0100

    added mobx and config

--- mobile-reactNative-mobx/App.js
+++ mobile-reactNative-mobx/App.js
@@ -1,10 +1,12 @@
 import { AppLoading } from 'expo';
+import { Provider } from 'mobx-react';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
+import Stores from './src/stores';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App(props) {
@@ -12,18 +14,22 @@ export default function App(props) {
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
+      <Provider {...Stores}>
+        <AppLoading
+          startAsync={loadResourcesAsync}
+          onError={handleLoadingError}
+          onFinish={() => handleFinishLoading(setLoadingComplete)}
+        />
+      </Provider>
     );
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <Provider {...Stores}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </Provider>
     );
   }
 }
